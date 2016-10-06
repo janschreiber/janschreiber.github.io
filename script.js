59e73463d3c79945aae7aa934746e9e2b9cd2891
Jan Schreiber <jan.schreiber@languagetool.org>
2016-10-06 13:35:04 +0200

    upload portfolio page

---
+++ script.js
@@ -0,0 +1,11 @@
+/* Set the height of the navigation bar to 24px */
+function openNav() {
+    document.getElementById("navbar").style.height = "24px";
+    document.getElementById("navbar").style.width = "100%";
+}
+
+/* Set the height of the navigation to 0 */
+function closeNav() {
+    document.getElementById("navbar").style.height = "0";
+    document.getElementById("navbar").style.width = "0";
+}
